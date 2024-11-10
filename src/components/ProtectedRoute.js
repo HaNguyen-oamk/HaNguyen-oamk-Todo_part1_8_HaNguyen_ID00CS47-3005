0e933bebd4d6c7943023b99276d33488e6ff55bc
Ha Nguyen <c3ngth01@student.oamk.fi>
2024-11-10 20:35:09 +0200

    last part 8

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -4,10 +4,6 @@ import { useUser } from "../context/useUser";
 
 export default function ProtectedRoute() {
   const { user } = useUser();
-
-  // If the user is not authenticated or doesn't have a valid token, redirect to "/signin"
   if (!user || !user.token) return <Navigate to="/signin" />;
-
-  // If authenticated, render the Outlet to display child routes
   return <Outlet />;
 }
